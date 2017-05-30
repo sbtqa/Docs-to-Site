b6d792bbc66df183b6fc924d4aaba0d81f9cd7f0
0I0u3uk <svetlakovanton@gmail.com>
2017-05-30 16:18:19 +0700

    Added syntax highlighting

--- src/main/java/ru/sbtqa/docs/DocToSite.java
+++ src/main/java/ru/sbtqa/docs/DocToSite.java
@@ -1,6 +1,10 @@
 package ru.sbtqa.docs;
 
+import org.w3c.dom.stylesheets.LinkStyle;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,6 +28,22 @@ public class DocToSite {
         }
     }
 
+    private static void extractFiles() throws IOException {
+        String path = System.getProperty("user.dir") + "/docs";
+
+        List<String> fileExtensions = new ArrayList<>();
+        fileExtensions.add(".js");
+        fileExtensions.add(".css");
+
+        for(String extension: fileExtensions) {
+            Map<String, String> files = FileHelper.readFiles(FileHelper.getFiles(extension));
+            for (Map.Entry<String, String> file : files.entrySet()) {
+                String filePath = path + "/" + file.getKey();
+                FileHelper.saveFile(filePath, file.getValue());
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         try {
@@ -34,6 +54,7 @@ public class DocToSite {
                     HtmlBuilder.getSampleTextWithMenu(mds), "##ArticleBody");
 
             saveHtmlFiles(result);
+            extractFiles();
             System.out.println("Docs is now in docs folder");
 
         } catch (Exception e) {
